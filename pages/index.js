2cc18defee05312d6196971de0bf4e627f8be271
Ryu Sato <ryu@weseek.co.jp>
2021-07-11 19:21:33 +0900

    バックエンドからデータを取得するようにした

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,37 @@
 import Head from 'next/head'
+import useSWR from 'swr'
+
+function Employees() {
+  const fetcher = () => fetch("http://localhost:8083/rest/v1/employees").then(res => res.json());
+  const { data, error } = useSWR('/employees', fetcher);
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
+  // データをレンダリングする
+  const employees = data;
+  const employeeListHtml = employees.map((employee) => 
+    <tr>
+      <td>{employee.id}</td>
+      <td>{employee.name}</td>
+      <td>{employee.department}</td>
+      <td>{employee.gender}</td>
+    </tr>
+  );
+  return (
+    <table>
+      <thead>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Department</th>
+        <th>Gender</th>
+      </thead>
+      <tbody>
+        {employeeListHtml}
+      </tbody>
+    </table>
+  );
+}
 
 export default function Home() {
   return (
@@ -8,7 +41,7 @@ export default function Home() {
       </Head>
 
       <main>
-        Hello world
+        <Employees />
       </main>
     </div>
   )
