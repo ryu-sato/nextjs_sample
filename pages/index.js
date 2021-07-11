399684c6c80b4edf22400546b069e6980482243d
Ryu Sato <ryu@weseek.co.jp>
2021-07-11 18:19:08 +0900

    next.js を初期化

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Employee management application (sample)</title>
+      </Head>
+
+      <main>
+        Hello world
+      </main>
+    </div>
+  )
+}
